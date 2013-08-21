1727b84d44ede4160b62fddfc3c3e70b0e5149b6
Bach Le <thebullno1@gmail.com>
2013-08-21 18:13:39 +0800

    Fix Set implementation

--- src/jaeger/Set.lua
+++ src/jaeger/Set.lua
@@ -1,41 +1,34 @@
 -- A set which allows addition of items during traversal
--- Good for holding a list of event subscribers
+-- Good for holding a list of event listeners
 
 local class = require "jaeger.Class"
 
 return class(..., function(i)
 	function i:__constructor()
 		self.items = {}
-		-- FIXME: WRONG, should be items to be added
-		self.removedItems = {}
+		self.itemsToAdd = {}
 	end
 
 	function i:add(item)
-		self.items[item] = true
-		self.removedItems[item] = nil
+		self.itemsToAdd[item] = true
 	end
 
 	function i:remove(item)
-		self.removedItems[item] = true -- only mark as removed
+		self.items[item] = nil
+		self.itemsToAdd[item] = nil
 	end
 
 	function i:hasItem(item)
-		return (not self.removedItems[item]) and self.items[item]
+		return self.items[item] or self.itemsToAdd[item]
 	end
 
 	function i:iterator()
-		-- remove marked item
-		for removedItem in pairs(self.removedItems) do
-			self.items[removedItem] = nil
-			self.removedItems[removedItem] = nil
+		-- add delayed items
+		for item in pairs(self.itemsToAdd) do
+			self.items[item] = true
+			self.itemsToAdd[item] = nil
 		end
 
 		return pairs(self.items)
-	end
-
-	function i:forEach(fun)
-		for item in self:iterator() do
-			fun(item)
-		end
 	end
 end)
