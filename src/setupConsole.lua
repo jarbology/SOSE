e779c9ff9e2696828f2e7bfbf15fa98b8d84bc36
Bach Le <thebullno1@gmail.com>
2013-08-22 20:45:19 +0800

    Add AssetManager shortcut var to console environment

--- src/setupConsole.lua
+++ src/setupConsole.lua
@@ -1,5 +1,6 @@
 entityMgr = engine:getSystem("jaeger.EntityManager")
 sceneMgr = engine:getSystem("jaeger.SceneManager")
+assetMgr = engine:getSystem("jaeger.AssetManager")
 
 function changeScene(scene, data)
 	sceneMgr:changeScene(scene, data)
