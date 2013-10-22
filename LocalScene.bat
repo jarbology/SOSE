cb9eec670543891d6061ae8bdafc655ee2a7cdb1
Bach Le <thebullno1@gmail.com>
2013-10-23 03:58:18 +0800

    Remove redundant launch scripts

--- LocalScene.bat
+++
@@ -1,3 +0,0 @@
-set FIRST_SCENE=scenes.NetworkTestScene
-set SCENE_DATA=nil
-call start-titan.bat
--- NetworkScene.bat
+++
@@ -1,3 +0,0 @@
-set FIRST_SCENE=scenes.NetworkTestScene
-set SCENE_DATA=127.0.0.1
-call start-titan.bat
