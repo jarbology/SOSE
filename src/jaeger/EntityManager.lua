918ee0b808b4ca9d0703c760a5009a1b7f87e675
Bach Le <thebullno1@gmail.com>
2014-04-11 03:44:21 +0800

    Remove the concept of managed components

--- src/jaeger/EntityManager.lua
+++ src/jaeger/EntityManager.lua
@@ -14,34 +14,16 @@ local Entity = class("jaeger.Entity", function(i)
 	function i:sendMessage(msg, ...)
 		-- TODO: would the order be indeterministic?
 		for _, component in ipairs(self.components) do
-			local manager = component.__manager
-
-			if manager then
-				local messageHandler = manager[msg]
-				if messageHandler then
-					messageHandler(manager, component, self, ...)
-				end
-			else
-				local messageHandler = component[msg]
-				if messageHandler then
-					messageHandler(component, ...)
-				end
+			local messageHandler = component[msg]
+			if messageHandler then
+				messageHandler(component, ...)
 			end
 		end
 
 		for _, component in ipairs(self.components) do
-			local manager = component.__manager
-
-			if manager then
-				local messageHandler = manager.onMessage
-				if messageHandler then
-					messageHandler(manager, component, self, msg, ...)
-				end
-			else
-				local messageHandler = component.onMessage
-				if messageHandler then
-					messageHandler(component, msg, ...)
-				end
+			local messageHandler = component.onMessage
+			if messageHandler then
+				messageHandler(component, msg, ...)
 			end
 		end
 	end
@@ -49,18 +31,9 @@ local Entity = class("jaeger.Entity", function(i)
 	-- Query an entity
 	function i:query(queryMsg, ...)
 		for _, component in ipairs(self.components) do
-			local manager = component.__manager
-
-			if manager then
-				local queryHandler = manager[queryMsg]
-				if queryHandler then
-					return queryHandler(manager, component, self, ...)
-				end
-			else
-				local queryHandler = component[queryMsg]
-				if queryHandler then
-					return queryHandler(component, ...)
-				end
+			local queryHandler = component[queryMsg]
+			if queryHandler then
+				return queryHandler(component, ...)
 			end
 		end
 	end
@@ -88,26 +61,10 @@ end)
 -- Tasks:
 -- * cleanup: really destroy entities
 return class(..., function(i)
-	-- Register a (managed) component type
-	-- Params:
-	-- * name: FQN of the component
-	-- * manager: the system that will manage components of this type
-	-- * methodName: the method to create this component
-	--               manager:<methodName>(entity, data) will be called to
-	--               create the component
-	function i:registerComponent(name, manager, methodName)
-		local componentFactories = self.componentFactories
-		assert(componentFactories[name] == nil, "Component type "..name.." is already registered")
-		componentFactories[name] = {manager, methodName}
-		print("Registered component type "..name)
-	end
-
 	-- Create an entity using a specification (table)
 	function i:createEntity(spec, overrides)
 		local entity = Entity.new()
 		overrides = overrides or {}
-		local componentFactories = self.componentFactories
-
 		for index, componentSpec in ipairs(spec) do
 			local componentType = componentSpec[1]
 			local componentSpecOverride = overrides[componentType] or {}
@@ -115,19 +72,10 @@ return class(..., function(i)
 				componentSpec[k] = v
 			end
 
-			local factory = componentFactories[componentType]
-			local component
-
-			if factory then
-				local manager, methodName = unpack(factory)
-				component = manager[methodName](manager, entity, componentSpec)
-				component.__manager = manager
-			else
-				self.scriptShortcut:enableShortcut(componentType)
-				local componentClass = require(componentType)
-				component = componentClass.new(componentSpec)
-				component.entity = entity
-			end
+			self.scriptShortcut:enableShortcut(componentType)
+			local componentClass = require(componentType)
+			local component = componentClass.new(componentSpec)
+			component.entity = entity
 			entity.components[index] = component
 			entity.components[componentType] = component
 		end
@@ -159,7 +107,6 @@ return class(..., function(i)
 
 	-- Private
 	function i:__constructor(config)
-		self.componentFactories = {}
 		self.destroyedEntities = {}
 		self.numDestroyedEntities = 0
 	end
