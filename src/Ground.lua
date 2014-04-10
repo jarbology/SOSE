efe3036511e258ccc578de0bf4e829ccede3e549
Bach Le <thebullno1@gmail.com>
2014-04-11 07:44:40 +0800

    Implement map dragging

--- src/Ground.lua
+++ src/Ground.lua
@@ -1,8 +1,11 @@
 local class = require "jaeger.Class"
 
 return class(..., function(i, c)
+	local widgetMgr = getSystem "jaeger.WidgetManager"
+
 	function i:__constructor(data)
 		self.zone = data.zone
+		self.camera = self.zone:query("getCamera")
 	end
 
 	function i:msgActivate()
@@ -15,9 +18,29 @@ return class(..., function(i, c)
 	end
 
 	function i:msgMouseLeft(x, y, down)
-		if not down then
+		if down then
+			self.clickCancelled = false
+			self.dragging = true
+			self.oldX, self.oldY = MOAIInputMgr.device.mouse:getLoc()
+			widgetMgr:grabFocus(self.entity)
+		else
+			self.dragging = false
+			widgetMgr:releaseFocus(self.entity)
+		end
+
+		if not self.clickCancelled and not down then
 			local tileX, tileY = self:worldToTile(x, y)
 			self.zone:sendMessage("msgTileClicked", tileX, tileY, x, y)
+		end
+	end
+
+	function i:msgMouseMoved(x, y)
+		if self.dragging then
+			self.clickCancelled = true
+			local x, y = MOAIInputMgr.device.mouse:getLoc()
+			self.camera:addLoc(self.oldX - x, y - self.oldY)
+			self.oldX = x
+			self.oldY = y
 		end
 	end
 
