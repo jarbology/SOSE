c1f39e6ba10627e1a69827356a296c039d1b9a6a
Bach Le <thebullno1@gmail.com>
2014-04-11 06:21:11 +0800

    Implement HostGame scene layout

---
+++ assets/gfx/sprites/ui/weaponBarActive.lua
@@ -0,0 +1,6 @@
+return {
+	atlas = "gui",
+	frameFormat = "UIdisplayitemhighlight.png",
+	xOrigin = 0,
+	yOrigin = 0
+}
--- src/scenes/HostGame.lua
+++ src/scenes/HostGame.lua
@@ -1,8 +1,13 @@
 local class = require "jaeger.Class"
 local RenderUtils = require "jaeger.utils.RenderUtils"
-local GameAnnouncer = require "GameAnnouncer"
 
 return class(..., function(i, c)
+	local LINK_SPEC = {
+		{ MOAIProp2D.INHERIT_LOC, MOAIProp2D.TRANSFORM_TRAIT }
+	}
+	local NAME_LEFT = {"happy", "jolly", "dreamy", "sad", "angry", "pensive", "focused", "sleepy", "grave", "distracted", "determined", "stoic", "stupefied", "sharp", "agitated", "cocky", "tender", "goofy", "furious", "desperate", "hopeful", "compassionate", "silly", "lonely", "condescending", "naughty", "kickass", "drunk", "boring", "nostalgic", "ecstatic", "insane", "cranky", "mad", "jovial", "sick", "hungry", "thirsty", "elegant", "backstabbing", "clever", "trusting", "loving", "suspicious", "berserk", "high", "romantic", "prickly", "evil"}
+	local NAME_RIGHT = {"lovelace", "franklin", "tesla", "einstein", "bohr", "davinci", "pasteur", "nobel", "curie", "darwin", "turing", "ritchie", "torvalds", "pike", "thompson", "wozniak", "galileo", "euclid", "newton", "fermat", "archimedes", "poincare", "heisenberg", "feynman", "hawking", "fermi", "pare", "mccarthy", "engelbart", "babbage", "albattani", "ptolemy", "bell", "wright", "lumiere", "morse", "mclean", "brown", "bardeen", "brattain", "shockley", "goldstine", "hoover", "hopper", "bartik", "sammet", "jones", "perlman", "wilson", "kowalevski", "hypatia", "goodall", "mayer", "elion", "blackwell", "lalande", "kirch", "ardinghelli", "colden", "almeida", "leakey", "meitner", "mestorf", "rosalind", "sinoussi", "carson", "mcclintock", "yonath"}
+
 	function i:__constructor()
 		local viewport = RenderUtils.newFullScreenViewport()
 		local background = RenderUtils.newLayer(viewport)
@@ -24,46 +29,136 @@ return class(..., function(i, c)
 
 	function i:start(engine, sceneTask)
 		local entityMgr = engine:getSystem("jaeger.EntityManager")
-		local sceneMgr = engine:getSystem("jaeger.SceneManager")
 
-		local announcer = GameAnnouncer.new(9001)
-		announcer:start(sceneTask, "SmallMap")
-		self.announcer = announcer
-
-		entityMgr:createEntity{
+		createEntity{
 			{"jaeger.Renderable", layer = self.layers.background },
 			{"jaeger.Background", texture = "bg1.png", width = 4000, height = 4000}
 		}
 
-		local sceneController = entityMgr:createEntity{
+		self.sceneController = createEntity{
 			{"jaeger.InlineScript",
 				msgBack = function()
-					sceneMgr:changeScene("scenes.MainMenu")
+					changeScene("scenes.MainMenu")
+				end,
+				msgChangeName = function()
+					self.gameName = c.randomName()
+					self.lblGameName:sendMessage("msgSetText", self.gameName)
+				end,
+				msgChangeMap = function(state, entity, id)
+					print(id)
+					local mapButtons = self.mapButtons
+					for _, btn in ipairs(mapButtons) do
+						btn:sendMessage("msgChangeSprite", "ui/weaponBar")
+					end
+
+					self.mapButtons[id]:sendMessage("msgChangeSprite", "ui/weaponBarActive")
 				end
 			}
 		}
 
-		local buttonTemplate = {
-			{"jaeger.Renderable", layer=self.layers.GUI},
-			{"jaeger.Widget", receiver = sceneController},
-			{"Button"},
-			{"jaeger.Text", rect={0, -50, 250, 0},
+		self:createButton(-420, -250, "Back", "msgBack")
+		self:createButton(420, -250, "Start", "msgStart")
+
+		--Window
+		local window = createEntity{
+			{"jaeger.Renderable", layer=self.layers.GUI, y = -20, xScale = 2.2, yScale=4.5},
+			{"jaeger.StretchPatch", name="dialog"}
+		}
+		local windowPivot = createEntity{
+			{"jaeger.Renderable", layer=self.layers.GUI}
+		}
+		window:sendMessage("msgAttach", windowPivot, LINK_SPEC)
+
+		local lbl = self:createLabel(0, 150, "Game name", {-125, -25, 125, 25})
+		windowPivot:sendMessage("msgAttach", lbl, LINK_SPEC)
+
+		local btnChangeName = createEntity{
+			{"jaeger.Renderable", layer=self.layers.GUI, y=100, xScale = 1.5, yScale=0.65},
+			{"jaeger.Widget", receiver=self.sceneController},
+			{"Button", message="msgChangeName"},
+			{"jaeger.StretchPatch", name="dialog"}
+		}
+		local name = c.randomName()
+		local label = self:createLabel(0, 0, name, {-125, -25, 125, 25})
+		btnChangeName:sendMessage("msgAttach", label, LINK_SPEC)
+		windowPivot:sendMessage("msgAttach", btnChangeName, LINK_SPEC)
+		self.gameName = name
+		self.lblGameName = label
+
+		local lbl = self:createLabel(0, 50, "Map name", {-125, -25, 125, 25})
+		windowPivot:sendMessage("msgAttach", lbl, LINK_SPEC)
+
+		--Maps
+		local  mapButtons = {}
+		local btn = self:createMapBtn(-200, 30, "Lams pam - Small", 1)
+		windowPivot:sendMessage("msgAttach", btn, LINK_SPEC)
+		mapButtons[1] = btn
+
+		local btn = self:createMapBtn(-200, -28, "Ruidem Dmal - Medium", 2)
+		windowPivot:sendMessage("msgAttach", btn, LINK_SPEC)
+		mapButtons[2] = btn
+
+		local btn = self:createMapBtn(-200, -86, "Eguh Ecalp - Large", 3)
+		windowPivot:sendMessage("msgAttach", btn, LINK_SPEC)
+		mapButtons[3] = btn
+		self.mapButtons = mapButtons
+
+		self.sceneController:sendMessage("msgChangeMap", 1)
+
+		--MOAIDebugLines.showStyle(MOAIDebugLines.TEXT_BOX)
+	end
+
+	function i:createButton(x, y, label, message)
+		local button = createEntity{
+			{"jaeger.Renderable", layer=self.layers.GUI, x=x, y=y, xScale = 0.75, yScale=0.75},
+			{"jaeger.Widget", receiver=self.sceneController},
+			{"Button", message=message},
+			{"jaeger.StretchPatch", name="dialog"}
+		}
+		local label = self:createLabel(0, 0, label)
+		button:sendMessage("msgAttach", label, LINK_SPEC)
+
+		return button
+	end
+
+	function i:createLabel(x, y, label, rect)
+		return createEntity{
+			{"jaeger.Renderable", layer=self.layers.GUI, x=x, y=y},
+			{"jaeger.Text", text=label,
+			                rect=rect or {-75, -25, 75, 25},
 			                font="karmatic_arcade.ttf",
-			                alignment = {MOAITextBox.LEFT_JUSTIFY, MOAITextBox.LEFT_JUSTIFY},
-			                size=40}
+			                alignment = {MOAITextBox.CENTER_JUSTIFY, MOAITextBox.CENTER_JUSTIFY},
+			                size=22}
+		}
+	end
+
+	function i:createMapBtn(x, y, name, mapId)
+		local button = createEntity{
+			{"jaeger.Renderable", layer=self.layers.GUI, x=x, y=y},
+			{"jaeger.Widget", receiver=self.sceneController},
+			{"Button", message="msgChangeMap", id=mapId},
+			{"jaeger.Sprite", spriteName="ui/weaponBar"}
 		}
 
-		entityMgr:createEntity(
-			buttonTemplate,
-			{
-				["jaeger.Renderable"] = { x=-1024/2, y=576/2},
-				["jaeger.Text"] = { text="Back" },
-				["Button"] = { message = "msgBack" }
-			}
-		)
+		local lbl = createEntity{
+			{"jaeger.Renderable", layer=self.layers.GUI},
+			{"jaeger.Text", text=name,
+			                rect={0, -55, 400, 0},
+			                font="karmatic_arcade.ttf",
+			                alignment = {MOAITextBox.RIGHT_JUSTIFY, MOAITextBox.CENTER_JUSTIFY},
+							size=18}
+		}
+		button:sendMessage("msgAttach", lbl, LINK_SPEC)
+
+		return button
 	end
 
 	function i:stop()
-		self.announcer:stop()
+	end
+
+	function c.randomName()
+		local left = NAME_LEFT[math.random(#NAME_LEFT)]
+		local right = NAME_RIGHT[math.random(#NAME_RIGHT)]
+		return left .."-"..right
 	end
 end)
